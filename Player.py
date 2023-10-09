f8767d385061e3dc7a93634b465d18917ebcb51b
Luisa Ramirez <133245727+LUISARAMIREZ08@users.noreply.github.com>
2023-10-08 23:35:17 -0500

    se borro el archivo Enemys.py

--- Player.py
+++ Player.py
@@ -33,6 +33,33 @@ class Player(pygame.sprite.Sprite):
     def move_left(self,pixels):
         if self.rect.x > 200:
             self.rect.x -= pixels
+    
+    def process_event_car(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                self.moving_right = True
+            if event.key == pygame.K_LEFT:
+                self.moving_left = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                self.moving_right = False
+            if event.key == pygame.K_LEFT:
+                self.moving_left = False
+
+    def move_car(self,event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                self.move_right(Settings.car_speed)
+            if event.key == pygame.K_LEFT:
+                self.move_left(Settings.car_speed)
+
+
+player = Player()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+enemy_sprites = pygame.sprite.Group()
+enemy_timer = 0
 
 class enemy_car(pygame.sprite.Sprite):
     def __init__(self, bad,lane):
@@ -64,6 +91,17 @@ class enemy_car(pygame.sprite.Sprite):
         else:
             self.kill()
 
+    def enemy_random():
+        global enemy_timer
+        enemy_timer += 1
+        if enemy_timer >= Settings.time_enemy: 
+            enemy_timer = 0
+            lane = random.choice([Settings.carril_1, Settings.carril_2, Settings.carril_3, Settings.carril_4])
+            enemy = enemy_car(random.randint(1, 5), lane)  # Crea un enemigo aleatorio
+            all_sprites.add(enemy)
+            enemy_sprites.add(enemy)
+
+
 aux = False
 collision_count = 0
 
@@ -85,43 +123,16 @@ def crash(value):
 #LOGICA DEL JUEGO
 def main_juego():
     game_over = False
-    moving_left = False
-    moving_right = False
-    player = Player()
-    all_sprites = pygame.sprite.Group()
-    all_sprites.add(player)
-    enemy_sprites = pygame.sprite.Group()
-    enemy_timer = 0
 
     while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            player.process_event_car(event) 
+        player.move_car(event)  
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    moving_right = True
-                if event.key == pygame.K_LEFT:
-                    moving_left = True
-
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    moving_right = False
-                if event.key == pygame.K_LEFT:
-                    moving_left = False
-        
-        if moving_left:
-            player.move_left(Settings.car_speed)
-        if moving_right:
-            player.move_right(Settings.car_speed)
-
-        enemy_timer += 1
-        if enemy_timer >= Settings.time_enemy: 
-            enemy_timer = 0
-            lane = random.choice([Settings.carril_1, Settings.carril_2, Settings.carril_3, Settings.carril_4])
-            enemy = enemy_car(random.randint(1, 5), lane)  # Crea un enemigo aleatorio
-            all_sprites.add(enemy)
-            enemy_sprites.add(enemy)
+        #Crea los enemigos
+        enemy_car.enemy_random()
 
         #Mueve los enemigos
         for enemy in enemy_sprites:
